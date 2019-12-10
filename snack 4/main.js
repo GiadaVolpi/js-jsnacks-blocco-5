361a992dd39c9816a33a5f6af97d3e1aa0496e04
Gi@da <giadavolpi8@gmail.com>
2019-12-10 17:30:00 +0100

    terminato snack 4

--- snack 4/main.js
+++ snack 4/main.js
@@ -1,3 +1,18 @@
 // Scrivi una funzione che fonda due array (con lo stesso numero di
 // elementi) prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+var array1 = [1,2,3,4,5,6];
+var array2 = ["Pippo", "Pluto", "Paperino", "Topolino", "Minnie", "Clarabella"];
+var arrayAlternato = [];
+
+for (var i = 0; i < array1.length; i++) {}
+
+for (var j = 0; j < array2.length; j++) {}
+
+for (var z = 0; z < array1.length; z++) {
+    arrayAlternato.push (array1[z]);
+    arrayAlternato.push (array2[z]);
+}
+
+console.log(arrayAlternato);
